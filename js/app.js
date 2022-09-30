12d0f1c071d406eacbb705b98b4332774ea25e90
Ivanovsky <ivanovsky777@gmail.com>
2022-09-30 02:00:56 -0500

    se agrega normalize, se cambia la fuente

--- js/app.js
+++ js/app.js
@@ -2,8 +2,6 @@ const iconoMenu = document.querySelector('#navbar-toggler'),
     menu = document.querySelector('#navbar-nav');
 
 iconoMenu.addEventListener('click', (e) => {
-
-
     menu.classList.toggle('active');
     document.body.classList.toggle('opacity');
     // Alternamos estilos para el menu y body
